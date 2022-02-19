df881d4e4efa8ea97f0e9e4d490d8c73a8ff9642
mario4vila <marioo.rab@gmail.com>
2022-02-19 20:17:50 +0100

    * Implementación IPN

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -16,7 +16,7 @@ class OrderController extends AbstractController
     {
     }
 
-    #[Route('/', name: 'orders', methods: ['GET'])]
+    #[Route('', name: 'orders', methods: ['GET'])]
     public function index(Request $request): Response
     {
         $status = $request->get('status', '');
@@ -64,7 +64,7 @@ class OrderController extends AbstractController
     }
 
     #[Route('/process_order_paypal', name: 'process_order_paypal', methods: ['GET'])]
-    public function processPaypal(Request $request, OrderStatusCompletedUpdater $orderStatusCompletedUpdater): Response
+    public function pdtPaypal(Request $request): Response
     {
         // Para cambiar al entorno de producción cambiar a: www.paypal.com en el archivo .env
         $paypalHostname = $this->getParameter('paypal.host');
@@ -139,22 +139,110 @@ class OrderController extends AbstractController
             return $this->create('El pago no ha sido completado.', 'danger');
         }
 
-        // Verificamos que el importe de pago y la moneda de pago sean correctos
-        if ($mc_currency != "EUR") {
-            return $this->create('La moneda no es la correcta.', 'danger');
-        }
-
-        // Comprobamos que txn_id no ha sido procesado previamente
-        $order = $this->orderRepository->findOneBy(['transactionId' => $txn_id]);
-        if ($order) {
-            return $this->create("Este pedido con el identificador [{$txn_id}] ya ha sido procesado.", 'info');
-        }
-
-        $orderStatusCompletedUpdater->execute($custom, $txn_id);
-
         $msg = "<h1>¡Hemos procesado tu pago exitosamente!</h1> 
             Recibimos {$mc_gross} Euros en concepto de: {$quantity} {$item_name}.<hr>
             Vuelve a comprar!";
         return $this->create($msg, 'success');
+    }
+
+    #[Route('/paypal_listener', name: 'paypal_listener', methods: ['POST'])]
+    public function listenerPaypal(Request $request, OrderStatusCompletedUpdater $orderStatusCompletedUpdater): Response
+    {
+        // Para cambiar al entorno de producción cambiar a: www.paypal.com en el archivo .env
+        $paypalHostname = $this->getParameter('paypal.host');
+
+        $raw_post_data = file_get_contents('php://input');
+
+        $raw_post_array = explode('&', $raw_post_data);
+        $myPost = array();
+
+        foreach ($raw_post_array as $keyval) {
+            $keyval = explode("=", $keyval);
+            if (count($keyval) == 2) {
+                $myPost[$keyval[0]] = urldecode($keyval[1]);
+            }
+        }
+
+        $req = 'cmd=_notify-validate';
+
+        if (function_exists('get_magic_quotes_gpc')) {
+            $get_magic_quotes_exists = true;
+        }
+
+        foreach ($myPost as $key => $value) {
+            if ($get_magic_quotes_exists == true) {
+                $value = urlencode(stripslashes($value));
+            } else {
+                $value = urlencode($value);
+            }
+            $req .= "&$key=$value";
+        }
+
+        $ch = curl_init("https://{$paypalHostname}/cgi-bin/webscr");
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 6);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/Cert/cacert.pem");
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'User-Agent: PHP-IPN-Verification-Script',
+            'Connection: Close',
+        ));
+
+        $response = curl_exec($ch);
+        if (!($response)) {
+            $errno = curl_errno($ch);
+            $errstr = curl_error($ch);
+            curl_close($ch);
+            throw new \Exception("cURL error: [$errno] $errstr");
+        }
+
+        $info = curl_getinfo($ch);
+        $httpCode = $info['http_code'];
+        if ($httpCode != 200) {
+            throw new \Exception("PayPal responded with http code $httpCode");
+        }
+
+        curl_close($ch);
+
+        if (strcmp($response, "INVALID") == 0) {
+            return new Response('INVALID');
+        }
+
+        if (strcmp($response, "VERIFIED") != 0) {
+            return new Response('NOT VERIFIED');
+        }
+
+        $payment_status    = $request->get('payment_status', '');
+        $txn_type          = $request->get('txn_type', '');
+        $txn_id            = $request->get('txn_id', '');
+        $mc_gross          = $request->get('mc_gross', 0.00);
+        $quantity          = $request->get('quantity', 0);
+        $item_name         = $request->get('item_name', '');
+        $custom            = (int)$request->get('custom', '');
+
+        if ($txn_type == "subscr_signup") {
+        } elseif ($txn_type == "subscr_payment" && $payment_status == "Completed") {
+        } elseif ($txn_type == "subscr_cancel") {
+        } elseif ($txn_type == 'web_accept' && $payment_status == 'Completed') {
+            $order = $this->orderRepository->findOneBy(['transactionId' => $txn_id]);
+            if ($order) {
+                return new Response("Este pedido con el identificador [{$txn_id}] ya ha sido procesado.");
+            }
+
+            $order = $orderStatusCompletedUpdater->execute($custom, $txn_id);
+            if (!$order) {
+                return new Response("No se encuentra el registro del pedido con el id [{$custom}] o no está en estado pendiente.");
+            }
+
+            return new Response("El pedido fue procesado exitosamente. \n");
+        }
+
+        return new Response('success');
     }
 }
